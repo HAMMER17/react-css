4f64c917fb5024c6206e39914bced8aaf980affa
HAMMER17 <plutalov70gmail.com>
2022-07-03 21:10:28 +0300

    chat

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function Email() {
+  const [email, setEmail] = React.useState('')
+  const [pass, setPass] = React.useState('')
+  function regist(e) {
+    e.preventDefault()
+    setEmail('')
+    setPass('')
+    console.log(email, pass)
+  }
   return (
     <>
       <div className='email'>
@@ -8,16 +16,17 @@ function Email() {
         <h1><i className="bi bi-envelope-fill"></i></h1>
       </div>
       <form action="" className='form'>
-        <label htmlFor="Email">Email</label>
-        <input type="text" placeholder='email' />
-
-        <label htmlFor="Password">Password</label>
-        <input type="text" placeholder='password' />
-
-        <button type="submit" className='button'>click</button>
+        <label htmlFor="Email">Email
+          <input type="text" placeholder='email' onChange={(e) => setEmail(e.target.value)} value={email} />
+        </label>
+        <label htmlFor="Password">Password
+          <input type="text" placeholder='password' onChange={(e) => setPass(e.target.value)} value={pass} />
+        </label>
+        <button type="submit" className='button' onClick={regist}>click</button>
       </form>
     </>
   )
 }
+
 
 export default Email
--- src/pages/Twitter.js
+++ src/pages/Twitter.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 function Twitter() {
+  const [chat, setChat] = React.useState([])
+  const [val, setVal] = React.useState('')
+  function addChat(e) {
+    e.preventDefault()
+    setChat([...chat, val], setVal(''))
+    console.log(val)
+  }
+  const result = chat.map((el, i) => {
+    if (i % 2 === 0) {
+      return <li key={i} className='key'>{el}</li>
+    } else
+      return <li key={i} className='key2'>{el}</li>
+
+  })
   return (
-    <div className='twitter'>
-      <h1>twitter</h1>
-      <h1><i className="bi bi-twitter"></i></h1>
-    </div>
+    <>
+      <div className='twitter'>
+        <h1>twitter</h1>
+        <h1><i className="bi bi-twitter"></i></h1>
+      </div>
+      <form action="" className='form'>
+        <input type="text" id='input' onChange={(e) => setVal(e.target.value)} value={val} />
+        <button className='button' onClick={addChat}>text</button>
+      </form>
+      <ul>{result}</ul>
+
+    </>
   )
 }
 
